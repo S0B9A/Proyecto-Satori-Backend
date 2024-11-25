b072c62de91342aea59c69bf48608dc0829815f1
Sebastian Arias <sebasxml9@gmail.com>
2024-11-24 22:16:13 -0600

    Se agrega la funcion de pusher en nuevas areas

--- models/PedidoModel.php
+++ models/PedidoModel.php
@@ -127,16 +127,15 @@ class PedidoModel
             $vSql = "INSERT INTO pedido (id_cliente, id_encargado, fecha, estado, metodo_entrega, direccion, costo, subtotal, impuesto, Observacion_combo, Observacion_pedido) " .
                 "VALUES ('$objeto->cliente_id', '$objeto->encargado_id', '$fechaBD', 'Pendiente de pago', '$objeto->tipo_pedido', '$objeto->indicaciones_ubicacion', '$objeto->total', '$objeto->subtotal', '$objeto->impuesto', '$objeto->ObservacionCombos', '$objeto->ObservacionProducto')";
 
-
             //Ejecutar la consulta
             $idPedido = $this->enlace->executeSQL_DML_last($vSql);
 
             //Insertar productos
             foreach ($objeto->productos as $item) {
                 $sql = "INSERT INTO pedidos_productos
-                    (id_pedido, id_producto, precio, cantidad, subtotal)
-                    VALUES
-                    ($idPedido, $item->id, $item->precio, $item->cantidad, $item->subtotal);";
+                (id_pedido, id_producto, precio, cantidad, subtotal)
+                VALUES
+                ($idPedido, $item->id, $item->precio, $item->cantidad, $item->subtotal);";
 
                 $vResultadoM = $this->enlace->executeSQL_DML($sql);
             }
@@ -144,43 +143,48 @@ class PedidoModel
             //Insertar combos
             foreach ($objeto->combos as $item) {
                 $sql = "INSERT INTO pedidos_combos
-                        (id_pedido, id_combo, precio, cantidad, subtotal)
-                        VALUES
-                        ($idPedido, $item->id, $item->precio, $item->cantidad, $item->subtotal);";
+                    (id_pedido, id_combo, precio, cantidad, subtotal)
+                    VALUES
+                    ($idPedido, $item->id, $item->precio, $item->cantidad, $item->subtotal);";
 
                 $vResultadoM = $this->enlace->executeSQL_DML($sql);
             }
 
+            // Obtener el pedido creado
+            $pedidoCreado = $this->get($idPedido);
+
+            // Configura Pusher
+            $options = [
+                'cluster' => 'us2', // Cambia esto por el cluster que te asigne Pusher
+                'useTLS' => true
+            ];
+            $pusher = new Pusher(
+                '6044eb48c974063b6561', // Reemplaza con tu clave de la aplicación
+                'c37b577f1bc42a492ac8', // Reemplaza con tu secreto de la aplicación
+                '1893850',             // Reemplaza con tu ID de la aplicación
+                $options
+            );
+
+            // Envía el evento a Pusher
+            $data = [
+                'pedido' => [
+                    'id' => $pedidoCreado->id,
+                    'estado' => $pedidoCreado->estado,
+                    'cliente_id' => $pedidoCreado->id_cliente,
+                    'costo' => $pedidoCreado->costo,
+                    'fecha' => $pedidoCreado->fecha
+                ]
+            ];
+
+            $pusher->trigger('pedido-channel', 'nuevo-pedido', $data);
+
             // Retornar el objeto creado
-            return $this->get($idPedido);
+            return $pedidoCreado;
         } catch (Exception $e) {
             die($e->getMessage());
         }
     }
 
-
-    /**
-     * Actualizar producto
-     * @param $objeto producto a actualizar
-     * @return $this->get($idproducto) - Objeto producto
-     */
-    //
-    public function updatePedidoPorPago($objeto)
-    {
-        try {
-
-            // Consulta SQL
-            $sql = "UPDATE pedido SET metodo_pago = '$objeto->metodo_pago', estado = 'Aceptada'  WHERE id = $objeto->id";
-
-            //Ejecutar la consulta
-            $cResults = $this->enlace->executeSQL_DML($sql);
-
-            //Retornar producto actualizado
-            return $this->get($objeto->id);
-        } catch (Exception $e) {
-            handleException($e);
-        }
-    }
 
 
     /**
@@ -218,10 +222,34 @@ class PedidoModel
                     'estado' => $pedidoActualizado->estado
                 ]
             ];
+
             $pusher->trigger('pedido-channel', 'estado-actualizado', $data);
 
             // Retornar pedido actualizado
             return $pedidoActualizado;
+        } catch (Exception $e) {
+            handleException($e);
+        }
+    }
+
+    /**
+     * Actualizar producto
+     * @param $objeto producto a actualizar
+     * @return $this->get($idproducto) - Objeto producto
+     */
+    //
+    public function updatePedidoPorPago($objeto)
+    {
+        try {
+
+            // Consulta SQL
+            $sql = "UPDATE pedido SET metodo_pago = '$objeto->metodo_pago', estado = 'Aceptada'  WHERE id = $objeto->id";
+
+            //Ejecutar la consulta
+            $cResults = $this->enlace->executeSQL_DML($sql);
+
+            //Retornar producto actualizado
+            return $this->get($objeto->id);
         } catch (Exception $e) {
             handleException($e);
         }
